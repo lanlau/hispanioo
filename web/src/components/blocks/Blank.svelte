e23c65ac0d50af9398f35d9e84912a3c19c2036e
lanlau <langlaislaurent@gmail.com>
2020-01-01 18:16:16 +0100

    fixing exercice error with special chars non taken into account

--- web/src/components/blocks/Blank.svelte
+++ web/src/components/blocks/Blank.svelte
@@ -20,8 +20,9 @@
   let value =
     $exerciceStore.answers[$exerciceStore.currentQuestion._key] &&
     $exerciceStore.answers[$exerciceStore.currentQuestion._key][content._key]
-      ? $exerciceStore.answers[$exerciceStore.currentQuestion._key][content._key]
-          .texte
+      ? $exerciceStore.answers[$exerciceStore.currentQuestion._key][
+          content._key
+        ].texte
       : "";
 
   $: onChange = () => {
@@ -54,8 +55,6 @@
     return false;
   };
 
-
-
   let specialChars = ["á", "í", "ó", "ú", "ü", "ñ", "¡", "¿"];
 
   const addSpecial = char => {
@@ -67,30 +66,29 @@
     ref.value = before + char + after;
     ref.selectionStart = ref.selectionEnd = start + char.length;
     ref.focus();
-    console.log(start, end);
+    value = ref.value;
+    onChange();
   };
-
-
 </script>
-<style>
 
-.input-container{
-  display:inline-block;
-  margin:0;
-  padding:0;
-}
+<style>
+  .input-container {
+    display: inline-block;
+    margin: 0;
+    padding: 0;
+  }
   input {
     padding: 5px;
   }
 
   .good {
     background-color: green;
-    color:#fff;
+    color: #fff;
   }
 
   .wrong {
     background-color: red;
-    color:#fff;
+    color: #fff;
   }
   input {
     border: 1px solid lightgrey;
@@ -136,36 +134,28 @@
     font-weight: 500;
     border: 1px solid lightblue;
   }
-
-
-
-
 </style>
+
 <div class="input-container">
-  <input 
-    on:change={onChange} 
-    bind:value={value}
-    bind:this={ref} 
+  <input
+    on:change={onChange}
+    bind:value
+    bind:this={ref}
     type="text"
-    class:good={checkIfGood()===true}
-    class:wrong={checkIfGood()===false}
-    
-  /> 
+    class:good={checkIfGood() === true}
+    class:wrong={checkIfGood() === false} />
 
-    <div class="specialcontainer">
-      <span class="specialtohover">Ω</span>
-      <div class="special">
-        {#each specialChars as char}
-          <a href="/" on:click|preventDefault={()=>addSpecial(char)}>{char}</a>
-        {/each}
-      </div>
+  <div class="specialcontainer">
+    <span class="specialtohover">Ω</span>
+    <div class="special">
+      {#each specialChars as char}
+        <a href="/" on:click|preventDefault={() => addSpecial(char)}>{char}</a>
+      {/each}
     </div>
-
-
-
+  </div>
 
   {#if $exerciceStore.results[$exerciceStore.currentQuestion._key] && $exerciceStore.results[$exerciceStore.currentQuestion._key].hasOwnProperty('isCorrect')}
-  <span class="correction">({content.text.replace('|',' ou ')})&nbsp;</span>
+    <span class="correction">({content.text.replace('|', ' ou ')})&nbsp;</span>
   {/if}
 
 </div>
